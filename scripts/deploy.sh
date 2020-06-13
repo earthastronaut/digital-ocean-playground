571beaa82f84dbfc2abc768fa420819a85eec774
earthastronaut <>
2020-06-12 19:35:48 -0600

    moved ssh config into deploy script because there are no secrets

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@
 # ---------------------------------------------------------------------------- #
 
 # Directory of this script.
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Secrets which should not be stored in git
 # {SECRETS_PATH}
@@ -14,7 +14,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #     ├── config            <- SSH config for defining github.com access
 #     ├── id_rsa            <- Private github deploy key referenced in config
 #     └── id_rsa.pub        <- Public key
-SECRETS_PATH="${SCRIPT_DIR}/untracked_production"
+SECRETS_PATH="${SCRIPT_PATH}/untracked_production"
 
 # Remote user@host
 REMOTE=$(cat ${SECRETS_PATH}/ssh_remote_host.txt)
@@ -49,10 +49,20 @@ set -e
 
 # -------------------------- #
 echo_section 'Copy SSH keys'
-run_copy_to_remote "${SECRETS_PATH}/ssh/*" '~/.ssh'
+
+TMP_SSH_CONFIG_FILE="${SCRIPT_PATH}/untracked_tmp_ssh_config"
+cat << EOF > ${TMP_SSH_CONFIG_FILE}
+HOST github.com
+    AddKeysToAgent yes
+    IdentityFile ~/.ssh/digital_ocean_github_deploy_key_rsa.priv
+EOF
+run_copy_to_remote "${TMP_SSH_CONFIG_FILE}" "~/.ssh/config"
+run_copy_to_remote "${SECRETS_PATH}/digital_ocean_github_deploy_key_rsa.priv" "~/.ssh/"
+rm ${TMP_SSH_CONFIG_FILE}
 
 # -------------------------- #
 echo_section 'Update git repository'
+
 response=$(run_remote_execute "ls ${REMOTE_CODE_PATH}")
 if [[ "$response" == "" ]]
 then
@@ -63,5 +73,6 @@ fi
 
 # -------------------------- #
 echo_section 'Docker-compose magic'
+
 run_remote_execute "docker-compose -f ${REMOTE_CODE_PATH}/docker-compose.yml down"
 run_remote_execute "docker-compose -f ${REMOTE_CODE_PATH}/docker-compose.yml up -d"
