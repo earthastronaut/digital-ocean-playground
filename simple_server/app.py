f7e9bdb07f1bab43017d652e6939ef4ecb0a7921
earthastronaut <>
2020-06-12 18:59:46 -0600

    add nginx proxy upstream of the webserver

--- simple_server/app.py
+++ simple_server/app.py
@@ -1,18 +1,28 @@
 import os
 
 from flask import Flask
-from flask import render_template
+from flask import (
+    render_template,
+    request,
+    make_response,
+)
 
 app = Flask('simple_server')
+
 
 @app.route('/')
 @app.route('/hello')
 @app.route('/hello/<user>')
 def index(user=None):
+    print(request.headers)
     context = {
         'username': user or 'world!',
     }
-    return render_template('index.html', **context)
+    response = make_response(render_template('index.html', **context))
+    response.headers.set(
+        'X-Verify-Proxy', request.headers.get('X-Verify-Proxy', 'proxy_off'))
+    return response
+
 
 if __name__ == "__main__":
     app.run()
